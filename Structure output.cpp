25db8155033e7a28ae941141b5c209b5607a2ca0
Dmitriy Loginov <brutalyso@gmail.com>
2022-09-21 13:25:36 +0700

    ADD

---
+++ Structure output.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Address
+{
+    string city;
+    string street;
+    string homeNum;
+    string apartamentNum;
+    string index;
+};
+
+void struct_output(Address out)
+{
+    cout << "Город: "          << out.city          << "\n" << 
+            "Улица: "          << out.street        << "\n" << 
+            "Номер дома: "     << out.homeNum       << "\n" << 
+            "Номер квартиры: " << out.apartamentNum << "\n" << 
+            "Индекс: "         << out.index         << "\n\n";
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    Address out1{ "Москва", "Арбат", "12", "8", "123456" };
+
+    struct_output(out1);
+
+    Address out2{ "Ижевск", "Пушкина", "59", "143", "953769" };
+
+    struct_output(out2);
+}
